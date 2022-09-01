ba6afb8c5501afa547fe1ce0713ee61794e36b76
IonAir1 <ionair.ironwood@gmail.com>
2022-09-01 19:29:08 +0800

    handle exceptions in CLI

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
-from fillnprint import FillNPrint
 import argparse
+import os
+from fillnprint import FillNPrint
 
 parser = argparse.ArgumentParser(prog="fillnprint")
 parser.add_argument("excel", help="excel input file")
@@ -10,12 +11,35 @@ parser.add_argument("-c","--cell", help="specify starting cell")
 parser.add_argument("-l","--limit", help="limit the ammount of rows to read")
 args = parser.parse_args()
 
-com = "FillNPrint('{}', '{}').generate('{}'".format(str(args.yaml), str(args.excel), str(args.output))
-if args.sheet != None:
-    com = com + ", sheet='{}'".format(str(args.sheet))
+fnp_inst = FillNPrint(args.yaml, args.excel)
+sheets = fnp_inst.get_sheets()
+
+#exception handling
+if not args.excel.endswith('.xlsx') or not os.path.exists(args.excel):
+    print("Invalid excel file")
+    exit()
+if fnp_inst.cfg == "error: invalid yaml file" or not os.path.exists(args.yaml):
+    print("Invalid yaml file")
+    exit()
+if not args.output.endswith('.pdf'):
+    print("Output file must be a pdf file")
+    exit()
+if not args.sheet is None and (not args.sheet in sheets and str(args.sheet).upper().isupper()):
+    print("Selected sheet is not a valid sheet")
+    exit()
+if not args.limit is None and str(args.limit).upper().isupper():
+        print("'Limit' setting must be an integer or left empty")
+        exit()
+
+com = "fnp_inst.generate('{}'".format(str(args.output))
+if not args.sheet is None:
+    if args.sheet.upper().isupper():
+        com = com + ", sheet='{}'".format(str(args.sheet))
+    else:
+        com = com + ", sheet=" + str(args.sheet)
 if args.cell != None:
     com = com + ", cell='{}'".format(str(args.cell))
 if args.limit != None:
-    com = com + ", cell={}".format(str(args.limit))
+    com = com + ", limit=" + str(args.limit)
 
 exec(com+')')
